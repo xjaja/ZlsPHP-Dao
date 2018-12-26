01d1d399171e7d740fafb0705e1e5309b5c22503
seekwe <soseek@vip.qq.com>
2018-12-26 12:45:18 +0800

    style

--- src/Create.php
+++ src/Create.php
@@ -6,13 +6,10 @@ use Z;
 
 /**
  * Zls.
- *
  * @author        影浅
  * @email         seekwe@gmail.com
- *
  * @copyright     Copyright (c) 2015 - 2017, 影浅, Inc.
- *
- * @see          ---
+ * @see           ---
  * @since         v0.0.1
  * @updatetime    2018-08-01 13:00
  */
@@ -20,10 +17,10 @@ class Create
 {
     public function bean($columns)
     {
-        $fields = [];
+        $fields        = [];
         $fieldTemplate = "    //{comment}\n    protected \${column0};";
         foreach ($columns as $value) {
-            $column = str_replace(' ', '', ucwords(str_replace('_', ' ', $value['name'])));
+            $column  = str_replace(' ', '', ucwords(str_replace('_', ' ', $value['name'])));
             $column0 = $value['name'];
             /*$column1 = lcfirst($column);*/
             $fields[] = str_replace(
@@ -45,20 +42,21 @@ class Create
     public function dao($columns, $table)
     {
         $primaryKey = '';
-        $_columns = [];
+        $_columns   = [];
         foreach ($columns as $value) {
             if ($value['primary']) {
                 $primaryKey = $value['name'];
             }
-            $_columns[] = '\''.$value['name']."'//".$value['comment'].PHP_EOL.'               ';
+            $_columns[] = '\'' . $value['name'] . "',//" . $value['comment'] . PHP_EOL . '              ';
         }
-        $columnsString = 'array('.PHP_EOL.'              '.implode(',', $_columns).')';
-        $code = "public function getColumns() {\n        return {columns};\n    }\n\n    public function getHideColumns() {\n        return array();\n    }\n\n    public function getPrimaryKey() {\n        return '{primaryKey}';\n    }\n\n    public function getTable() {\n        return '{table}';\n    }\n";
+        $columnsString = '[' . PHP_EOL . '              ' . implode('', $_columns) . ']';
+        $code          = "public function getColumns() {\n        return {columns};\n    }\n\n    public function getHideColumns() {\n        return [];\n    }\n\n    public function getPrimaryKey() {\n        return '{primaryKey}';\n    }\n\n    public function getTable() {\n        return '{table}';\n    }\n";
         if (false !== strpos(z::getOpt(1), 'bean')) {
             $code .= "\n    public function getBean() {\n        return parent::getBean();\n    }\n";
         }
         $code = str_replace(['{columns}', '{primaryKey}', '{table}'], [$columnsString, $primaryKey, $table], $code);
+        $methods = ['getColumns', 'getHideColumns', 'getPrimaryKey', 'getTable', 'getBean'];
 
-        return $code;
+        return [$code, $primaryKey ? '' : 'Did not find the primary key, please fill in manually.', $methods];
     }
 }
